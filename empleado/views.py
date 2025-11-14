0da05f50b6680f94b03a4515e1ef8372a9973320
Katherine <katherinesuazo032@gmail.com>
2025-11-14 17:44:45 -0300

    Agregar formulario de registro de empleados y actualizar vistas correspondientes

--- empleado/views.py
+++ empleado/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import Empleado
-
+from .forms import EmpleadoForm
 
 def lista_empleados(request):
-    empleados = Empleado.objects.all()
-    return render(request, 'empleado/ingresoEmpleado.html', {'empleados': empleados})
+    context = {}
 
+    def get(self, request):
+        context['formulario'] = EmpleadoForm
+        empleados = Empleado.objects.all()
+        # return render(request, 'empleado/ingresoEmpleado.html', {'empleados': empleados})
+        return render(request, 'empleado/ingresoEmpleado.html', self.context)
 
+    def post(self, request):
+        context['formulario'] = EmpleadoForm
+        empleados = Empleado.objects.all()
+        return render(request, 'empleado/ingresoEmpleado.html', self.context)
