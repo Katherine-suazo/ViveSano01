e74a7d3f949b9e5ba3924accd24822f1fc87fb39
Katherine <katherinesuazo032@gmail.com>
2025-11-16 23:00:23 -0300

    Agregar vista y plantilla para la lista de clientes, actualizar rutas y mejorar la navegación en la página de inicio

--- cliente/urls.py
+++ cliente/urls.py
@@ -4,4 +4,5 @@ from cliente import views
 
 urlpatterns = [
     path('', views.registro_clientes, name = 'registro_cliente'),
+    path('listaClientes/', views.lista_clientes, name = 'lista_clientes'),
 ]
