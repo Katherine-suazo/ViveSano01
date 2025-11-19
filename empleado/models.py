855c0c8796ab0d7e11caecfa1ae61e39dac61c1f
Katherine-suazo <katherinesuazo032@gmail.com>
2025-11-19 18:22:44 -0300

    Modificar modelo de empleados y hacer cambios en plantillas

--- empleado/models.py
+++ empleado/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+ROLES_CHOISES = [
+    ('ENCARGADO_TIENDA', 'Encargado de tienda'),
+    ('ENCARGADO_LOGISTICA', 'Encargado de logistica'),
+    ('ATENCION_CLIENTE', 'Atencion a cliente'),
+]
 
 
 class Empleado(models.Model):
@@ -6,8 +13,12 @@ class Empleado(models.Model):
     apellido_empleado = models.CharField(max_length=100, null = False, blank = False)
     usuario_empleado = models.CharField(max_length=100, null = False, blank = False)
     contrasena_empleado = models.CharField(max_length=255, null = False, blank = False)
-    rol_empleado = models.CharField(max_length=50, null = False, blank = False)
+    rol_empleado = models.CharField(max_length=50, choices = ROLES_CHOISES, null = False, blank = False)
 
     def __str__(self):
-        return f"{self.nombre_empleado} {self.apellido_empleado}"
+        return f"{self.nombre_empleado} {self.apellido_empleado} - {self.rol_empleado}"
     
+# 1. crear modelo
+# 2. crear vistas
+# 3. crear urls
+# 4. crear plantillas
